7951ddf694aa234db0c7a942f5890a7fd738406a
Rajaboff <rajabov.manuchehr2001@gmail.com>
2022-10-03 19:10:25 +0500

    initialize and add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ChatRoomsComponent } from './components/chat-rooms/chat-rooms.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatSidebarComponent } from './components/chat-sidebar/chat-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SidebarComponent,
+    ChatRoomsComponent,
+    ChatComponent,
+    ChatSidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/bars.interface.ts
@@ -0,0 +1,5 @@
+export interface IBar {
+  icon: string,
+  label: string,
+  active: boolean
+}
